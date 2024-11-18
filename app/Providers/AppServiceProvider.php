7002b7390a4d5b73be11a6368560fb0beb058de9
mehdi <mehdialibakhshi03@gmail.com>
2024-11-18 10:41:24 +0330

    edit https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 use App\Models\Setting;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
         $setting = Setting::first();
         $lang = app()->getLocale();
         \view()->share(compact('setting','lang'));
